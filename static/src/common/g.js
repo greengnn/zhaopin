72b17715831257c58f959ecf0afc73c536783f1c
zhangjianbin <zhangjianbin@xiaomi.com>
2015-08-17 00:11:45 +0800

    add

--- static/src/common/g.js
+++ static/src/common/g.js
@@ -7,20 +7,20 @@ seajs.config({
   // 调试模式
   debug: true,
   // Sea.js 的基础路径
-  base: 'http://127.0.0.1:8080/'
+  base: 'http://127.0.0.1:8080/static/'
 });
 
 
-var rem = (function (doc, win) {
-  var docEl = doc.documentElement,
-    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-    recalc = function () {
-      var clientWidth = docEl.clientWidth;
-      if (!clientWidth) return;
-      docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
-    };
+// var rem = (function (doc, win) {
+//   var docEl = doc.documentElement,
+//     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+//     recalc = function () {
+//       var clientWidth = docEl.clientWidth;
+//       if (!clientWidth) return;
+//       docEl.style.fontSize = 10 * (clientWidth / 320) + 'px';
+//     };
 
-  if (!doc.addEventListener) return;
-  win.addEventListener(resizeEvt, recalc, false);
-  doc.addEventListener('DOMContentLoaded', recalc, false);
-})(document, window);
+//   if (!doc.addEventListener) return;
+//   win.addEventListener(resizeEvt, recalc, false);
+//   doc.addEventListener('DOMContentLoaded', recalc, false);
+// })(document, window);
